bd42379e1670a264e0e82aa812a0210d3c983f60
williamlox <williamlox@yahoo.com.tw>
2021-01-10 22:00:12 +0800

    20210110 Add public

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,27 +8,29 @@ use Illuminate\Support\Facades\Auth;
 
 class MainController extends Controller
 {
-    function index(){
+    public function index(){
         $titles = Headlines::all();
         return view("index",compact('titles'));
     }
 
-    function insert(Request $req){
+    public function insert(Request $req){
+    if ($req->isMethod('post')){  
         $title = $req ->input('title');
         //if($title)長度>0才新增
         $data = new Headlines;
         $data ->title = $title;
         $data ->save();
+        }
         return redirect("/");
     }
 
-    function delete($id){
+    public function delete($id){
         $data = Headlines::find($id);
         $data->delete();
         return redirect('/');
      }
 
-    function logout(){
+    public function logout(){
         Auth::logout();
         return redirect("/");
     }
